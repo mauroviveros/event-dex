54a23da6cc70f89d6c3e61b56e97d60b5a4a34c6
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-01-30 16:34:03 -0300

    refactor: :wheelchair: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,14 @@ import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [icon(), react()]
+  integrations: [icon(), react()],
+  adapter: vercel()
 });
